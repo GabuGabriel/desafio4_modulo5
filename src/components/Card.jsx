d66da495c1f00d30b0f3e5cc2f7ceb430dffe301
gmunoz <gmunoz@wit-labs.com>
2023-11-20 12:14:20 -0300

    cambioguardado

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,14 @@ export default function Card() {
     <img src="" alt="" />
     <div className="carddetail">
       <h2>pizza name</h2>
+      <hr />
+      <ul>
+        <li>pizza ingredientes</li>
+      </ul>
+      <hr />
+      <p>Precio:</p>
+      <button>detalle</button>
+      <button>agregar al carrito</button>
     </div>
   </div>
 }
