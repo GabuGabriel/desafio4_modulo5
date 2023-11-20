44be9808fa4da20abccabe1b0fdf88c157ca8967
gmunoz <gmunoz@wit-labs.com>
2023-11-20 17:42:42 -0300

    detallesvarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-/* import Detalle from './views/Detalle' */
+import Pizza from './views/Pizza'
 import Home from './views/Home'
 import Pagina404 from './views/404'
-/* import { Provider } from './Contexto/PokeContexto' */
+import { Provider } from './Contexto/PizzaContexto'
 import Carrito from './views/Carrito'
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Provider> */}
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-       {/*  <Route path='/Detalle' element={<Detalle />} /> */}
-        <Route path='/Carrito' element={<Carrito />} />
-        <Route path='*' element={<Pagina404 />} />
-      </Routes>
-      {/* </Provider> */}
+      <Provider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Pizza' element={<Pizza />} />
+          <Route path='/Carrito' element={<Carrito />} />
+          <Route path='*' element={<Pagina404 />} />
+        </Routes>
+      </Provider>
     </BrowserRouter>
   )
 }
--- src/Contexto/PizzaContexto.jsx
+++ src/Contexto/PizzaContexto.jsx
@@ -4,6 +4,10 @@ export const PizzaContexto = createContext()
 
 export const Provider = ({ children }) => {
   const [pizzas, setPizzas] = useState([])
+  const [carrito, setCarrito] = useState([])
+  const [precio, setPrecio] = useState([])
+  const [precioTotal, setPrecioTotal] = useState(0)
+
   const getData = async () => {
     const res = await fetch('./pizzas.json')
     const data = await res.json()
@@ -15,7 +19,13 @@ export const Provider = ({ children }) => {
   }, [])
 
   const globalState = {
-    pizzas
+    pizzas,
+    carrito,
+    precio,
+    setCarrito,
+    setPrecio,
+    precioTotal,
+    setPrecioTotal
   }
 
   return (
